93a7ccb7950a25b4420de78c86ad2ba23799bd9a
joshunrau <contact@joshuaunrau.com>
2024-07-01 17:37:06 -0400

    implement echo

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,23 @@
+use std::collections::VecDeque;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     pub fn alert(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn exec(command: &str) -> String {
+    let mut args: VecDeque<&str> = {
+        let mut v = Vec::new();
+        for elem in command.trim().split_whitespace() {
+            v.push(elem);
+        }
+        v.into()
+    };
+    let program = args.pop_front().unwrap_or("");
+    match program {
+        "echo" => Vec::from(args).join(" "),
+        _ => String::from(&format!("wshell: command not found: {program}")),
+    }
 }
